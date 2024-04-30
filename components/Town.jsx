25b400a281e85f82ea8b6ad64bcb5f731fc4d211
ahrirubin <ahri.rubin@gmail.com>
2024-04-30 11:20:20 +0200

    town.jsx update

--- components/Town.jsx
+++ components/Town.jsx
@@ -13,21 +13,14 @@ const TownComponent = () => {
 
   return (
     <div className="flex items-center gap-2 my-2">
-      <div className="w-4/5"> 
+      <div className="w-4/5">
         <label className="inline-flex flex-col w-full">
-					<span className="text-gray-600">Select Town</span>
+          <span className="text-gray-600">Select Town</span>
           <Dropdown
-            options={[
-              "Eldorium",
-              "Verdant Haven",
-              "Vaporia",
-              "Marinath",
-              "Aetherius",
-              "Arcanum",
-            ]}
+            options={["Stoneholm", "Pineview", "Marinport"]}
             onSelect={handleSelect}
             value={hometown}
-            />
+          />
         </label>
         <p className="text-gray-600">Selected Town: {hometown}</p>
       </div>
@@ -38,35 +31,19 @@ const TownComponent = () => {
           tooltipText={
             <div style={{ maxWidth: "400px" }}>
               <p>
-                Eldorium:
-                <br />A city ablaze with ambition and innovation, its towering
-                spires reflecting the fiery determination of its people.
+                Stoneholm:
+                <br />
+                The crisp mountain air of Stoneholm whispers secrets of
+                adventure.
               </p>
               <p>
-                Verdant Haven:
-                <br />A tranquil sanctuary nestled within ancient forests, where
-                magic and nature intertwine in harmony.
+                Pineview:
+                <br />A tranquil sanctuary nestled within ancient forests.
               </p>
               <p>
-                Vaporia:
-                <br />A city veiled in perpetual mist and powered by arcane
-                steam, stands as a testament to innovation and ingenuity amidst
-                a landscape of swirling gears and shimmering copper spires.
-              </p>
-              <p>
-                Marinath:
-                <br />A coastal city where sea and sky meet, shaping a culture
-                of maritime exploration and tradition.
-              </p>
-              <p>
-                Aetherius:
-                <br />A city of serene heights and spiritual wisdom, where the
-                wind's gentle touch guides the way.
-              </p>
-              <p>
-                Arcanum:
-                <br />A city where arcane knowledge and protection intertwine
-                amidst its mystical streets.
+                Marinport:
+                <br />A small harbor perfect for seafaring quests and trading
+                riches.
               </p>
             </div>
           }
