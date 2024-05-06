b767eee2b85cbb915c65109883c0e146e77f4c63
ahrirubin <ahri.rubin@gmail.com>
2024-05-06 23:55:23 +0200

    Nav Component and character select

--- app/(auth)/lore/page.js
+++ app/(auth)/lore/page.js
@@ -7,53 +7,81 @@ import RaceComponent from "@/components/Race";
 import ClassComponent from "@/components/Class";
 import TownComponent from "@/components/Town";
 import Button from "@/components/Button";
-import { FaTimesCircle} from "react-icons/fa";
+import { FaTimesCircle } from "react-icons/fa";
+import NavBar from "@/components/NavBar";
 
 export default function LorePage() {
-	const router = useRouter();
+  <NavBar />;
+  const router = useRouter();
 
-	const { name, setName, characterClass, race, hometown } = useLore();
+  const { name, setName, characterClass, race, hometown } = useLore();
 
-	const [error, setError] = useState("");
+  const [error, setError] = useState("");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!name || !characterClass || !race || !hometown) {
-			setError("All fields need to be filled in");
-			return;
-		}
-		try {
-			router.push("/chat");
-		} catch (error) {
-			setError("There was a small problem: ", error);
-			console.log(error);
-		}
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !characterClass || !race || !hometown) {
+      setError("All fields need to be filled in");
+      return;
+    }
+    try {
+      router.push("/chat");
+    } catch (error) {
+      setError("There was a small problem: ", error);
+      console.log(error);
+    }
+  };
 
-	return (
-		<>
-			<div className="flex my-8 gap-3">
-				<div className="w-1/2 bg-slate-50 p-4">
-					<h1>Lore</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-					</p>
-				</div>
-				<div className="w-1/2 bg-slate-50 p-4">
-					<form className="flex flex-col gap-1" onSubmit={handleSubmit}>
-						<label className="block">
-							<span className="text-gray-600">Add your name</span>
-							<input className="mt-1 block w-full" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
-						</label>					
-						<RaceComponent />
-						<ClassComponent />
-						<TownComponent />
-						<Button type="submit">Submit</Button>
-					</form>
-					{error && <div className="bg-red-100 text-red-800 mt-3 flex p-3 align-middle gap-3 items-center"><FaTimesCircle /> {error}</div>}
-				</div>
-			</div>
-			
-		</>
-	);
+  return (
+    <>
+      <nav class="flex gap-8 py-2 px-5 items-center justify-between bg-gray-200">
+        <div class="flex gap-8 items-center ">
+          <button>Logo</button>
+        </div>
+
+        <div class="flex  space-x-4">
+          <button class=" hover:text-gray-500">Link</button>
+          <button class=" hover:text-gray-500">Link</button>
+          <button class="bg-black text-white px-5 py-2 hover:bg-text-gray-500">
+            Play
+          </button>
+        </div>
+      </nav>
+
+      <div className="flex my-8 gap-3">
+        <div className="w-1/2 bg-slate-50 p-4">
+          <h1>Lore</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="w-1/2 bg-slate-50 p-4">
+          <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+            <label className="block">
+              <span className="text-gray-600">Add your name</span>
+              <input
+                className="mt-1 block w-full"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </label>
+            <RaceComponent />
+            <ClassComponent />
+            <TownComponent />
+            <Button type="submit">Submit</Button>
+          </form>
+          {error && (
+            <div className="bg-red-100 text-red-800 mt-3 flex p-3 align-middle gap-3 items-center">
+              <FaTimesCircle /> {error}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
