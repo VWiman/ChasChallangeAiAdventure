b7e66871eac19abd6ebd20b6863e68227a2c84fb
ozayo <ozay.ozdemir@gmail.com>
2024-05-10 04:06:34 +0200

    Added 2 new component

--- app/page.js
+++ app/page.js
@@ -1,50 +1,15 @@
 "use client";
-import Button from "@/components/Button";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { FaCcStripe } from "react-icons/fa";
+import HowWork from "@/components/HowWork";
+import MainHero from "@/components/MainHero";
+
 
 export default function Home() {
-  const router = useRouter();
-  const handleValidation = () => {
-    router.push("/validate");
-  };
-
-  const [teaserFact, setTeaserFact] = useState();
-
-  const fact = ["graceful", "brave", "elegant", "mystical"];
-
-  const fetchTeaserFact = () => {
-    const randomIndex = Math.floor(Math.random() * fact.length);
-    setTeaserFact(fact[randomIndex]);
-  };
-
-  useEffect(() => {
-    fetchTeaserFact();
-    const timer = setInterval(() => {
-      fetchTeaserFact();
-    }, 3000);
-
-    return () => clearInterval(timer);
-  }, []);
 
   return (
 		// Note that the main for the other pages is in layout under auth
-		<main className="container mx-auto max-w-[810px] px-3">
-			<div>
-				<h1>Begin your {teaserFact} adventure today! </h1>
-
-				<p>
-					Welcome to the gateway of your own epic saga. Here, you are not just a visitor but the very heart of a grand
-					adventure. Craft your hero, shape your destiny, and dive into a realm where your choices forge the path ahead.
-					With every decision, the story unfolds, guided by an enigmatic AI storyteller. Are you ready to discover the
-					unknown, face mystical challenges, and emerge as the architect of your own legendary tale? Step forward and
-					transform your imagination into an unforgettable narrative.
-				</p>
-				<Button radius="rsm" onClick={handleValidation}>
-					Get started
-				</Button>
-			</div>
+    <main className="">
+      <MainHero />
+      <HowWork />
 		</main>
 	);
 }
