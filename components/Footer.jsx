e6b949f56d19cb32f2e329103e0259458a6bb2fc
ozayo <ozay.ozdemir@gmail.com>
2024-05-10 04:57:39 +0200

    edit footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,10 @@ export default function Footer() {
 					<p> 2024 - &copy; AI Adventure</p>
 				</div>
 				<div className="flex gap-2">
-					<Link href="/">Home</Link>
-					<Link href="/validate">Start Game</Link>
-					<Link href="/">Help</Link>
-					<Link href="/">About</Link>
+					<Link className="hover:text-primary" href="/">Home</Link>
+					<Link className="hover:text-primary" href="/validate">Start Game</Link>
+					<Link className="hover:text-primary" href="/">Help</Link>
+					<Link className="hover:text-primary" href="/">About</Link>
 				</div>
 			</div>
 		</footer>
