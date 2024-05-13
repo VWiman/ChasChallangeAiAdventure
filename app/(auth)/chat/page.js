e41acc1a7ad7ba943bf21a6d3fbc868d45282320
why_code <119089018+VWiman@users.noreply.github.com>
2024-05-13 13:45:45 +0200

    The AI should now not ask questions but rather only show suggestions. I added loading as text while loading response. I added "eye" to toggle responses on and off.

--- app/(auth)/chat/page.js
+++ app/(auth)/chat/page.js
@@ -4,280 +4,260 @@ import { useApi } from "@/context/ApiContext";
 import { useLore } from "@/context/LoreContext";
 import { useEffect, useRef, useState } from "react";
 import NavBar from "@/components/NavBar";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 export default function Chat() {
-  <NavBar />;
+	<NavBar />;
 
-  // Global
-  const { apiKey } = useApi();
-  const { name, characterClass, race, hometown, hometownDescription } = useLore();
+	// Global
+	const { apiKey } = useApi();
+	const { name, characterClass, race, hometown, hometownDescription } = useLore();
 
-  // Local
-  const [message, setMessage] = useState(
+	// Local
+	const [message, setMessage] = useState(
 		`This is my first time. My characters name is ${name}, I am a ${characterClass}, I am from the ${race} race, My hometown is ${hometown} ${hometownDescription}.`
 	);
-  const [response, setResponse] = useState("");
-  const [history, setHistory] = useState(["user: " + message]);
-  // Full history is used only for summary button, send it during chat.
-  const [fullHistory, setFullHistory] = useState("");
-  const [fullUserHistory, setFullUserHistory] = useState([message]);
-  const [fullSystemHistory, setFullSystemHistory] = useState([]);
-  const [displayHistory, setDisplayHistory] = useState([]);
-  const [suggestions, setSuggestions] = useState([]);
-  const hasSentInitialMessage = useRef(false);
-  const [isWaiting, setIsWaiting] = useState(false);
-  const [summary, setSummary] = useState("");
+	const [response, setResponse] = useState("");
+	const [history, setHistory] = useState(["user: " + message]);
+	// Full history is used only for summary button, send it during chat.
+	const [fullHistory, setFullHistory] = useState("");
+	const [fullUserHistory, setFullUserHistory] = useState([message]);
+	const [fullSystemHistory, setFullSystemHistory] = useState([]);
+	const [displayHistory, setDisplayHistory] = useState([]);
+	const [suggestions, setSuggestions] = useState([]);
+	const hasSentInitialMessage = useRef(false);
+	const [isWaiting, setIsWaiting] = useState(false);
+	const [summary, setSummary] = useState("");
+	const [hideUserResponse, setHideUserResponse] = useState(false);
 
-  const sendMessage = async (userMessage = message) => {
-    if (!apiKey) {
-      alert("API Key is not set.");
-      return;
-    }
+	const sendMessage = async (userMessage = message) => {
+		if (!apiKey) {
+			alert("API Key is not set.");
+			return;
+		}
 
-    if (!userMessage) {
-      alert("Please enter a message.");
-      return;
-    }
+		if (!userMessage) {
+			alert("Please enter a message.");
+			return;
+		}
 
-    setFullUserHistory((prevFullUserHistory) => {
-      const newFullUserHistory =
-        prevFullUserHistory.length >= 1 ? prevFullUserHistory : "";
-      return [...newFullUserHistory, userMessage];
-    });
+		setFullUserHistory((prevFullUserHistory) => {
+			const newFullUserHistory = prevFullUserHistory.length >= 1 ? prevFullUserHistory : "";
+			return [...newFullUserHistory, userMessage];
+		});
 
-    setHistory((prevHistory) => {
-      const newHistory =
-        prevHistory.length >= 4 || prevHistory.length === 1
-          ? prevHistory.slice(1)
-          : prevHistory;
-      return [...newHistory, " user: " + userMessage];
-    });
+		setHistory((prevHistory) => {
+			const newHistory = prevHistory.length >= 4 || prevHistory.length === 1 ? prevHistory.slice(1) : prevHistory;
+			return [...newHistory, " user: " + userMessage];
+		});
 
-    const content = JSON.stringify({
-      context: {
-        character: {
-          name: name,
-          class: characterClass,
-          race: race,
-        },
-        summary: "Summary of recent and current events",
-        location: "Current location",
-        world: {
-          type: "fantasy",
-          characterHometown: hometown,
-        },
-        message: "Response to " + userMessage,
-      },
-      suggestions: "Provide suggestions of what to do next",
-    });
+		const content = JSON.stringify({
+			context: {
+				character: {
+					name: name,
+					class: characterClass,
+					race: race,
+				},
+				summary: "Summary of recent and current events",
+				location: "Current location",
+				world: {
+					type: "fantasy",
+					characterHometown: hometown,
+				},
+				message: "Response to " + userMessage,
+			},
+			suggestions: "Provide suggestions of what to do next",
+		});
 
-    try {
-      setIsWaiting(true);
-      const response = await fetch(
-        "https://api.openai.com/v1/chat/completions",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${apiKey}`,
-          },
-          body: JSON.stringify({
-            // NOTE GPT VERSION IS 4 NOW IN USE
-            model: "gpt-4-1106-preview",
-            response_format: { type: "json_object" },
-            temperature: 0.0,
-            max_tokens: 400,
-            messages: [
-              {
-                role: "user",
-                content: message,
-              },
-              {
-                role: "system",
-                content:
-                  "You are a dungeon master, you guide the user on an adventure. The user is assuming the role of a character.",
-              },
-              {
-                role: "system",
-                content: "History log: " + history.toString(),
-              },
-              {
-                role: "system",
-                content: `The user characters hometown: ${hometown}, race: ${race}, name: ${name} and class: ${characterClass} can not be changed. Update context based on history, reply and user message each time to reflect what is going on.`,
-              },
-              {
-                role: "system",
-                content:
-                  "Tell the user what happens from a second person perspective, and present 3 suggestions for what to do next.",
-              },
-              {
-                role: "system",
-                content:
-                  "You always reply with a JSON object. You do not include qoutation marks or literals in response.",
-              },
-              {
-                role: "system",
-                content: content,
-              },
-            ],
-          }),
-        }
-      );
+		try {
+			setIsWaiting(true);
+			const response = await fetch("https://api.openai.com/v1/chat/completions", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${apiKey}`,
+				},
+				body: JSON.stringify({
+					// NOTE GPT VERSION IS 4 NOW IN USE
+					model: "gpt-4-1106-preview",
+					response_format: { type: "json_object" },
+					temperature: 0.0,
+					max_tokens: 400,
+					messages: [
+						{
+							role: "user",
+							content: message,
+						},
+						{
+							role: "system",
+							content:
+								"You are a dungeon master, you guide the user on an adventure. The user is assuming the role of a character.",
+						},
+						{
+							role: "system",
+							content: "History log: " + history.toString(),
+						},
+						{
+							role: "system",
+							content: `The user characters hometown: ${hometown}, race: ${race}, name: ${name} and class: ${characterClass} can not be changed. Update context based on history, reply and user message each time to reflect what is going on.`,
+						},
+						{
+							role: "system",
+							content:
+								"Tell the user what happens from a second person perspective, and present 3 suggestions for what to do next. Dont pose the questions in your text response, only present suggestions in suggestions.",
+						},
+						{
+							role: "system",
+							content:
+								"You always reply with a JSON object. You do not include qoutation marks or literals in response.",
+						},
+						{
+							role: "system",
+							content: content,
+						},
+					],
+				}),
+			});
 
-      const data = await response.json();
+			const data = await response.json();
 
-      if (response.ok) {
-        const fullMessageObject = JSON.parse(data.choices[0].message.content);
-        console.log("Full message object:", fullMessageObject);
-        setResponse(fullMessageObject.context.message);
-        setSuggestions(fullMessageObject.suggestions);
-        setFullSystemHistory((prevFullSystemHistory) => {
-          return [...prevFullSystemHistory, fullMessageObject.context.message];
-        });
-        setHistory((prevHistory) => {
-          const newHistory =
-            prevHistory.length >= 4 ? prevHistory.slice(1) : prevHistory;
-          return [
-            ...newHistory,
-            "system: " + fullMessageObject.context.message,
-          ];
-        });
-        setMessage("");
-        setIsWaiting(false);
-      } else {
-        setIsWaiting(false);
-        throw new Error(
-          data.error.message || "Failed to fetch response from OpenAI"
-        );
-      }
-    } catch (error) {
-      console.error("API request failed.");
-      setResponse("Failed to send message. Please try again.");
-    }
-    hasSentInitialMessage.current = true;
-  };
+			if (response.ok) {
+				const fullMessageObject = JSON.parse(data.choices[0].message.content);
+				console.log("Full message object:", fullMessageObject);
+				setResponse(fullMessageObject.context.message);
+				setSuggestions(fullMessageObject.suggestions);
+				setFullSystemHistory((prevFullSystemHistory) => {
+					return [...prevFullSystemHistory, fullMessageObject.context.message + "\n"];
+				});
+				setHistory((prevHistory) => {
+					const newHistory = prevHistory.length >= 4 ? prevHistory.slice(1) : prevHistory;
+					return [...newHistory, "system: " + fullMessageObject.context.message];
+				});
+				setMessage("");
+				setIsWaiting(false);
+			} else {
+				setIsWaiting(false);
+				throw new Error(data.error.message || "Failed to fetch response from OpenAI");
+			}
+		} catch (error) {
+			console.error("API request failed.");
+			setResponse("Failed to send message. Please try again.");
+		}
+		hasSentInitialMessage.current = true;
+	};
 
-  /* 
+	/* 
 	-- Initial message using useEffect --
 	Important: In production we should remove hasSentInitialMessage and timeout as strict mode is no longer needed. At the moment we need it to not call api twice on first page load.
 	*/
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (!hasSentInitialMessage.current) {
-        sendMessage().then(() => {
-          hasSentInitialMessage.current = true;
-        });
-      }
-    }, 1500);
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			if (!hasSentInitialMessage.current) {
+				sendMessage().then(() => {
+					hasSentInitialMessage.current = true;
+				});
+			}
+		}, 100);
 
-    return () => clearTimeout(timeout);
-  }, []);
+		return () => clearTimeout(timeout);
+	}, []);
 
-  // Logging history
-  useEffect(() => {
-    console.log("Recent history: ", history);
-    console.log("Full user history: ", fullUserHistory);
-    console.log("Full system history: ", fullSystemHistory);
-    console.log("Full history: ", fullHistory);
-  }, [history, fullUserHistory]);
+	// Logging history
+	useEffect(() => {
+		console.log("Recent history: ", history);
+		console.log("Full user history: ", fullUserHistory);
+		console.log("Full system history: ", fullSystemHistory);
+		console.log("Full history: ", fullHistory);
+	}, [history, fullUserHistory]);
 
-  useEffect(() => {
-    setFullHistory(fullHistory + response + "\n");
-    setDisplayHistory((prevDisplayHistory) => {
-      const newDisplayHistory = prevDisplayHistory;
-      return [
-        ...newDisplayHistory,
-        <p className="py-2 leading-tight" key={response.slice(0, 10)}>
-          {response}
-        </p>,
-      ];
-    });
-  }, [fullSystemHistory]);
+	useEffect(() => {
+		setFullHistory(fullHistory + response + "\n");
+		setDisplayHistory((prevDisplayHistory) => {
+			const newDisplayHistory = prevDisplayHistory;
+			return [...newDisplayHistory, <p key={response.slice(0, 10)}>{response}</p>];
+		});
+	}, [fullSystemHistory]);
 
-  useEffect(() => {
-    setFullHistory((prevFullHistory) => {
-      const newFullHistory =
-        fullUserHistory.length >= 2 ? prevFullHistory + message + "\n" : "";
-      return newFullHistory;
-    });
+	useEffect(() => {
+		setFullHistory((prevFullHistory) => {
+			const newFullHistory = fullUserHistory.length >= 2 ? prevFullHistory + message + "\n" : "";
+			return newFullHistory;
+		});
 
-    setDisplayHistory((prevDisplayHistory) => {
-      const newDisplayHistory = prevDisplayHistory;
-      return [
-        ...newDisplayHistory,
-        <p className="py-2 leading-tight text-red-300" key={message.slice(0, 10)}>
-          {message}
-        </p>,
-      ];
-    });
-  }, [fullUserHistory]);
+		setDisplayHistory((prevDisplayHistory) => {
+			const newDisplayHistory = prevDisplayHistory;
+			return [
+				...newDisplayHistory,
+				<p className="text-red-500" key={message.slice(0, 10)}>
+					{message}
+				</p>,
+			];
+		});
+	}, [fullUserHistory]);
 
-  function handleSuggestion(suggestion) {
-    setMessage(suggestion);
-    sendMessage(suggestion);
-  }
+	function handleSuggestion(suggestion) {
+		setMessage(suggestion);
+		sendMessage(suggestion);
+	}
 
-  const handleSendSummary = async () => {
-    console.log("Starting fetch...");
-    try {
-      const response = await fetch(
-        "https://api.openai.com/v1/chat/completions",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${apiKey}`,
-          },
-          body: JSON.stringify({
-            model: "gpt-4-1106-preview",
-            temperature: 0.0,
-            max_tokens: 2000,
-            messages: [
-              {
-                role: "user",
-                content: fullHistory,
-              },
-              {
-                role: "system",
-                content:
-                  "You are a fantasy novel author. Summarize into a short-story. Your response should not include any formating.",
-              },
-            ],
-          }),
-        }
-      );
+	const handleSendSummary = async () => {
+		console.log("Starting fetch...");
+		try {
+			const response = await fetch("https://api.openai.com/v1/chat/completions", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${apiKey}`,
+				},
+				body: JSON.stringify({
+					model: "gpt-4-1106-preview",
+					temperature: 0.0,
+					max_tokens: 2000,
+					messages: [
+						{
+							role: "user",
+							content: fullHistory,
+						},
+						{
+							role: "system",
+							content:
+								"You are a fantasy novel author. Summarize into a short-story. Your response should not include any formating.",
+						},
+					],
+				}),
+			});
 
-      const data = await response.json();
+			const data = await response.json();
 
-      if (
-        response.ok &&
-        data.choices &&
-        data.choices.length > 0 &&
-        data.choices[0].message.content
-      ) {
-        const fullSummary = JSON.stringify(data.choices[0].message.content);
-        const newFullSummary = fullSummary
-          .slice(1, -1)
-          .replace(/\\n/g, "\n")
-          .replace(/\\/g, "");
-        setSummary(newFullSummary);
-      } else {
-        throw new Error(
-          (data.error && data.error.message) ||
-            "Failed to fetch response from OpenAI"
-        );
-      }
-    } catch (error) {
-      console.error("API request failed:", error);
-      setResponse("Failed to send message. Please try again.");
-    }
-  };
+			if (response.ok && data.choices && data.choices.length > 0 && data.choices[0].message.content) {
+				const fullSummary = JSON.stringify(data.choices[0].message.content);
+				const newFullSummary = fullSummary.slice(1, -1).replace(/\\n/g, "\n").replace(/\\/g, "");
+				setSummary(newFullSummary);
+			} else {
+				throw new Error((data.error && data.error.message) || "Failed to fetch response from OpenAI");
+			}
+		} catch (error) {
+			console.error("API request failed:", error);
+			setResponse("Failed to send message. Please try again.");
+		}
+	};
 
-  return response != "" ? (
+	return response != "" ? (
 		<div>
 			<div className="flex flex-col m-auto mt-20 justify-center items-center text-lg w-[930px]">
 				<h1>{name}´s adventure</h1>
-				{displayHistory.slice(5)}
+				<p className="text-sm self-end">Toggle response <Button radius="rf" size="small" onClick={() => setHideUserResponse(!hideUserResponse)}>
+					{hideUserResponse ? (
+						<FiEye />
+					) : (
+						<FiEyeOff />
+					)}
+				</Button></p>
+				
+				<section className="py-2 leading-tight">
+					{hideUserResponse ? fullSystemHistory : displayHistory.slice(5)}
+				</section>
+
 				<ul className="flex flex-col my-10 gap-2">
 					{suggestions &&
 						suggestions.map((suggestion, index) => (
