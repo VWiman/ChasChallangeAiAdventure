01135dec41680bd528117584adb92251b4cd6c9f
ahrirubin <ahri.rubin@gmail.com>
2024-05-14 15:03:11 +0200

    Updated how to text

--- components/HowWork.jsx
+++ components/HowWork.jsx
@@ -6,24 +6,44 @@ const HowWork = () => {
       </div>
       <div className="cards grid gap-4 sm:grid-cols-2 sm:grid-rows-2 grid-cols-1 grid-rows-1">
         <div className="hcard bg-cardbg">
-          <h3><span>1</span>API validation</h3>
-          <p>Alias quaerat natus, voluptatum sit id non? Sapiente ipsum aliquid, dolore.</p>
+          <h3>
+            <span>1</span>API validation
+          </h3>
+          <p>Input the API key provided by OpenAI to access the service.</p>
         </div>
         <div className="hcard bg-cardbg">
-          <h3><span>2</span>World selection</h3>
-          <p>Alias quaerat natus, voluptatum sit id non? Sapiente ipsum aliquid, dolore perferendis soluta excepturi autem iure vel dolorem nihil impedit a incidunt.</p>
+          <h3>
+            <span>2</span>Character creation
+          </h3>
+          <p>
+            Create your very own unique character by selecting from a diverse
+            array of options. With four distinct choices each for races,
+            classes, and character homes, the possibilities are truly endless.
+          </p>
         </div>
         <div className="hcard bg-cardbg">
-          <h3><span>3</span>Character creation</h3>
-          <p>Alias quaerat natus, voluptatum sit id non? Sapiente ipsum aliquid, dolore perferendis soluta excepturi autem iure vel dolorem nihil impedit a incidunt.</p>
+          <h3>
+            <span>3</span>World selection
+          </h3>
+          <p>
+            Where does your character's story originate? Are they a humble
+            fisherman from Marinport, or perhaps an intrepid adventurer from
+            Stoneholm in pursuit of their next quest?
+          </p>
         </div>
         <div className="hcard bg-cardbg">
-          <h3><span>4</span>Adventure await</h3>
-          <p>Alias quaerat natus, voluptatum sit id non? Sapiente ipsum aliquid, dolore perferendis soluta excepturi autem iure vel dolorem nihil impedit a incidunt.</p>
+          <h3>
+            <span>4</span>Adventure awaits
+          </h3>
+          <p>
+            Embark on a personalized adventure with your character, where every
+            choice you make shapes the journey ahead. Experience unique rewards
+            and consequences tailored to your actions along the way.
+          </p>
         </div>
       </div>
     </div>
-	);
+  );
 };
 
 export default HowWork;
