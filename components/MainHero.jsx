49ac329d09f11fb21bc0f05ac03df242f53fd2b9
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-05-15 08:35:22 +0200

    Changed Button on LAnding page to Link

--- components/MainHero.jsx
+++ components/MainHero.jsx
@@ -1,16 +1,8 @@
 "use client";
-import Button from "@/components/Button";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-
+import Link from "next/link";
 
 export default function MainHero() {
-
-  const router = useRouter();
-  const handleValidation = () => {
-    router.push("/validate");
-  };
-
   const [teaserFact, setTeaserFact] = useState();
 
   const fact = ["graceful", "brave", "elegant", "mystical"];
@@ -28,28 +20,35 @@ export default function MainHero() {
 
     return () => clearInterval(timer);
   }, []);
-  
 
-
-	return (
-			<section className="bg-cover bg-center min-h-[600px] w-full flex items-center" style={{ 
-        backgroundImage: ` linear-gradient(0deg, rgba(28,25,23,1) 6%, rgba(28,25,23,0) 63%), url('./images/new_dark_hero.png')`
-      }}> 
-        <div className="mycontainer">
-          <div className="flex flex-col gap-8 items-center text-center">
-            <h1>Begin your {teaserFact} adventure today! </h1>
-            <p>
-              Welcome to the gateway of your own epic saga. Here, you are not just a visitor but the very heart of a grand
-              adventure. Craft your hero, shape your destiny, and dive into a realm where your choices forge the path ahead.
-              With every decision, the story unfolds, guided by an enigmatic AI storyteller. Are you ready to discover the
-              unknown, face mystical challenges, and emerge as the architect of your own legendary tale? Step forward and
-              transform your imagination into an unforgettable narrative.
-            </p>
-            <Button size="large w-80" radius="rsm" onClick={handleValidation}>
-              Start your adventure
-            </Button>
-          </div>
+  return (
+    <section
+      className="bg-cover bg-center min-h-[600px] w-full flex items-center"
+      style={{
+        backgroundImage: ` linear-gradient(0deg, rgba(28,25,23,1) 6%, rgba(28,25,23,0) 63%), url('./images/new_dark_hero.png')`,
+      }}
+    >
+      <div className="mycontainer">
+        <div className="flex flex-col gap-8 items-center text-center">
+          <h1>Begin your {teaserFact} adventure today! </h1>
+          <p>
+            Welcome to the gateway of your own epic saga. Here, you are not just
+            a visitor but the very heart of a grand adventure. Craft your hero,
+            shape your destiny, and dive into a realm where your choices forge
+            the path ahead. With every decision, the story unfolds, guided by an
+            enigmatic AI storyteller. Are you ready to discover the unknown,
+            face mystical challenges, and emerge as the architect of your own
+            legendary tale? Step forward and transform your imagination into an
+            unforgettable narrative.
+          </p>
+          <Link
+            className="bg-primary text-darktext hover:bg-accent hover:text-btnhovertext inline-block w-80 px-10 py-5 rounded"
+            href="/validate"
+          >
+            Start your adventure
+          </Link>
         </div>
-			</section>
-	);
+      </div>
+    </section>
+  );
 }
